5b48a8a24f70ef35230796a4dc6d24d4afe166c6
yin1991 <84140478+xiaokuili@users.noreply.github.com>
2023-03-03 08:58:43 +0000

    init project

---
+++ gene_ca/gene.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+
+# 使用命令 "mkdir -p" 可以创建嵌套的目录，即使上级目录不存在
+mkdir -p ./ca
+
+
+rm -rf ./ca/*
+
+
+
+# 生成 CA 根证书
+openssl genrsa -out ./ca/ca.key 2048
+openssl req -new -x509 -key ./ca/ca.key -sha256 -subj "/CN=CA Root" -out ./ca/ca.crt
+wait
+
+# 生成客户端证书
+openssl genrsa -out ./ca/client.key 2048
+openssl req -new -key ./ca/client.key -out ./ca/client.csr -subj "/CN=hsmap.com"
+openssl x509 -req -in ./ca/client.csr -CA ./ca/ca.crt -CAkey ./ca/ca.key -CAcreateserial -out ./ca/client.crt -sha256
+wait
+
+# 生成服务器证书
+openssl genrsa -out ./ca/server.key 2048
+openssl req -new -key ./ca/server.key -out ./ca/server.csr -subj "/CN=hsmap.com"
+openssl x509 -req -in ./ca/server.csr -CA ./ca/ca.crt -CAkey ./ca/ca.key -CAcreateserial -out ./ca/server.crt -sha256
+wait
+
+# 删除临时文件
+rm  ./ca/client.csr ./ca/server.csr
+
+# 设置文件权限（可选）
+chmod 600 ./ca/*.key
+chmod 644 ./ca/*.crt
+
+# 分发公钥
+cp ./ca/*.crt ../client/
+cp ./ca/*.crt ../server/
+cp ./ca/*.crt ../root/
+
+# 分发私钥
+cp ./ca/client.key ../client/
+cp ./ca/server.key ../server/
+cp ./ca/ca.key ../root/
