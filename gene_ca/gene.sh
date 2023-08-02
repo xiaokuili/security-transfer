0dfd8fbb81488a0e023d36551213dcc2e62035ea
SWEET-BS <75962709+SWEET-BS@users.noreply.github.com>
2023-08-02 17:04:24 +0800

    Update gene.sh
    证书有效时长

--- gene_ca/gene.sh
+++ gene_ca/gene.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-
+#DAY 是证书有效时长 以天为单位
+DAY=$1
 # 使用命令 "mkdir -p" 可以创建嵌套的目录，即使上级目录不存在
 mkdir -p ./ca
 
@@ -9,20 +10,20 @@ rm -rf ./ca/*
 
 
 # 生成 CA 根证书
-openssl genrsa -out ./ca/ca.key 2048
-openssl req -new -x509 -key ./ca/ca.key -sha256 -subj "/CN=CA Root" -out ./ca/ca.crt
+openssl genrsa -out ./ca/ca.key 2048 
+openssl req -new -x509 -key ./ca/ca.key -sha256 -subj "/CN=CA Root" -out ./ca/ca.crt -days $DAY
 wait
 
 # 生成客户端证书
-openssl genrsa -out ./ca/client.key 2048
-openssl req -new -key ./ca/client.key -out ./ca/client.csr -subj "/CN=hsmap.com"
-openssl x509 -req -in ./ca/client.csr -CA ./ca/ca.crt -CAkey ./ca/ca.key -CAcreateserial -out ./ca/client.crt -sha256
+openssl genrsa -out ./ca/client.key 2048 
+openssl req -new -key ./ca/client.key -out ./ca/client.csr -subj "/CN=hsmap.com" -days $DAY
+openssl x509 -req -in ./ca/client.csr -CA ./ca/ca.crt -CAkey ./ca/ca.key -CAcreateserial -out ./ca/client.crt -sha256 -days $DAY
 wait
 
 # 生成服务器证书
-openssl genrsa -out ./ca/server.key 2048
-openssl req -new -key ./ca/server.key -out ./ca/server.csr -subj "/CN=hsmap.com"
-openssl x509 -req -in ./ca/server.csr -CA ./ca/ca.crt -CAkey ./ca/ca.key -CAcreateserial -out ./ca/server.crt -sha256
+openssl genrsa -out ./ca/server.key 2048 
+openssl req -new -key ./ca/server.key -out ./ca/server.csr -subj "/CN=hsmap.com" -days $DAY
+openssl x509 -req -in ./ca/server.csr -CA ./ca/ca.crt -CAkey ./ca/ca.key -CAcreateserial -out ./ca/server.crt -sha256 -days $DAY
 wait
 
 # 删除临时文件
@@ -41,3 +42,4 @@ cp ./ca/*.crt ../root/
 cp ./ca/client.key ../client/
 cp ./ca/server.key ../server/
 cp ./ca/ca.key ../root/
+
